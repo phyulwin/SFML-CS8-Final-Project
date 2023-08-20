2a9aec013e90f9a8005a65f1502e6c98bb5a540a
Phyu Lwin (Kelly) <phyulwin73929@gmail.com>
2023-08-20 13:50:36 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,51 @@
+/**
+ * @file main.cpp
+ * @brief This file contains the main function that initializes the graphical interface and handles user interactions.
+ */
+
 #include <iostream>
 #include <cstdlib>
-#include <SFML\Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
-#include "Interface.h"
+#include "Interface.h" // Include the Interface class definition
 
+/**
+ * @brief The main entry point of the application.
+ * @return An integer indicating the exit status of the application.
+ */
 int main() {
-    srand((unsigned) time(NULL));
+    srand((unsigned) time(NULL)); // Seed the random number generator
 
+    // Create a window with specified dimensions and title
     sf::RenderWindow window(sf::VideoMode(1500, 844, 32), "Window Desktop");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(60); // Set the maximum framerate for the window
 
-    Interface interface;
-    //save sample test files
+    Interface interface; // Create an instance of the Interface class
+
+    // Save sample test files to the interface
     interface.saveFile("Quick-Notes", "Math equation.txt", "1+2=4");
     interface.saveFile("Quick-Notes", "English.txt", "I am an Apple.");
-    //save sample deleted files
+
+    // Add sample deleted files to the interface
     interface.addDeletedFile("Hello World.txt", "hello world");
     interface.addDeletedFile("Empty Note.txt", "no data");
 
-    while(window.isOpen())
-    {
+    // Main loop for handling events and updating the interface
+    while (window.isOpen()) {
         sf::Event event;
-        while(window.pollEvent(event)){
-            if (event.type == sf::Event::Closed){
-                window.close();
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close(); // Close the window if the close event is triggered
             }
-            interface.addEventHandler(window, event);
+            interface.addEventHandler(window, event); // Handle events for the interface
         }
-        interface.update();
+        
+        interface.update(); // Update the interface
 
-        window.clear(sf::Color::Black);
-        window.draw(interface);
-        window.display();
+        window.clear(sf::Color::Black); // Clear the window with a black background
+        window.draw(interface); // Draw the interface components
+        window.display(); // Display the contents of the window
     }
-    return 0;
+    
+    return 0; // Return 0 to indicate successful program execution
 }
